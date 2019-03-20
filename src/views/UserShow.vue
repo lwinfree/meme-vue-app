819dbbfc406e42cb3f3b320d6bdcc4f3d7fbb242
lilly <lillywinfree@gmail.com>
2019-03-20 10:24:27 -0700

    adds style to favorites on user profile

--- src/views/UserShow.vue
+++ src/views/UserShow.vue
@@ -1,20 +1,63 @@
 <template>
   <div class="users-show">
-    <div class = "container">
-      <h1>{{ user.name }}</h1>
-      <h4>Email: {{ user.email }}</h4>
-      <h4>Zipcode: {{ user.zipcode }}</h4>
-      <h4>Favorites:</h4>
-      <div v-for="favorite in user.favorites">
-        <router-link v-bind:to="'/pets/' + favorite.petfinder_id">
-          <h4>{{favorite.pet_info.pet_name}}</h4>
-          <h4>{{favorite.petfinder_id}}</h4>
-          <img :src="favorite.pet_info.pet_photo">
-        </router-link>
-      </div>
+    <div  id="page-content" class = "container">
+
+      <section id="project">
+
+        <div class="section-description light">
+          <h1>{{ user.name }}</h1>
+          <h4>
+            <i class="fa fa-envelope"></i> {{ user.email }}
+          </h4>
+          <h4>
+            <i class="fa fa-map-marker"></i> {{ user.zipcode }}
+          </h4>
+          <h2 class="space-top-30">Favorites:</h2>
+        </div>
+      </section>
+
+      <section id="portfolio" class="info-box two-col">
+        <div class = "container">
+          <ul class="row portfolio list-unstyled lightbox" id="grid" >
+            <!-- <div v-for="pet in pets"> -->
+              <!-- <router-link v-bind:to="'/pets/' + pet.pet_id"> -->
+                
+                <li class="col-xs-6 project m-project" v-for="favorite in user.favorites">
+                    <div class="img-bg-color primary">
+                        <!-- / project-link -->
+                        <img :src="favorite.pet_info.pet_photo" :alt="favorite.pet_info.pet_name">
+                        <!-- / project-image -->
+
+                        <!-- project-hover-tools -->
+                        <div class="project-hover-tools">
+                            <router-link v-bind:to="'/pets/' + favorite.petfinder_id" class="view-btn">
+                                <i class="lnr lnr-eye"></i>
+                            </router-link>
+                        </div><!-- / project-hover-tools -->
+
+                        <!-- project-details -->
+                        <div class="project-details">
+                            <h5 class="project-title">{{ favorite.pet_info.pet_name }}</h5>
+                        </div><!-- / project-details -->
+                    </div><!-- / img-bg-color -->
+                </li>
+
+
+              <!-- </router-link> -->
+            <!-- </div> -->
+          </ul>
+        </div>
+      </section>
+        
+      <router-link v-bind:to="'/users/me/edit'">Edit my profile</router-link> | 
+      <router-link v-bind:to="'/pets'">Search for pets</router-link>
+      
     </div>
-    <router-link v-bind:to="'/users/me/edit'">Edit my profile</router-link> | 
-    <router-link v-bind:to="'/pets'">Search for pets</router-link>
+
+
+
+    
+
   </div>
 </template>
 
